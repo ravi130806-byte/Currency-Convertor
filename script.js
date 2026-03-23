1d107c41e444e490e2e481f359d3237c521b949f
Ravi Kant <ravi130806@gmail.com>
2026-03-23 20:20:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Select elements
 const fromSelect = document.querySelector(".from select");
 const toSelect = document.querySelector(".to select");
 const fromImg = document.querySelector(".from img");
@@ -7,8 +6,6 @@ const msg = document.querySelector(".msg");
 const button = document.querySelector("button");
 const amountInput = document.querySelector(".amount input");
 const exchangeIcon = document.querySelector(".fa-arrow-right-arrow-left");
-
-// Currency → Country Code Mapping
 const countryList = {
   USD: "US",
   INR: "IN",
@@ -21,78 +18,52 @@ const countryList = {
   AED: "AE",
   SAR: "SA"
 };
-
-// API URL
 const BASE_URL = "https://api.exchangerate-api.com/v4/latest/";
-
-// 🔹 Populate Dropdowns
 for (let currency in countryList) {
   let option1 = document.createElement("option");
   option1.value = currency;
   option1.innerText = currency;
-
   let option2 = option1.cloneNode(true);
-
   fromSelect.append(option1);
   toSelect.append(option2);
 }
-
-// Set Default Selected
 fromSelect.value = "USD";
 toSelect.value = "INR";
-
-// 🔹 Update Flag Function
 function updateFlag(selectElement, imgElement) {
   let currencyCode = selectElement.value;
   let countryCode = countryList[currencyCode];
 
   imgElement.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
 }
-
-// Initial Flag Set
 updateFlag(fromSelect, fromImg);
 updateFlag(toSelect, toImg);
-
-// 🔹 Change Event for Flags
 fromSelect.addEventListener("change", () => {
   updateFlag(fromSelect, fromImg);
 });
-
 toSelect.addEventListener("change", () => {
   updateFlag(toSelect, toImg);
 });
-
-// 🔹 Exchange Icon Swap
 exchangeIcon.addEventListener("click", () => {
   let temp = fromSelect.value;
   fromSelect.value = toSelect.value;
   toSelect.value = temp;
-
   updateFlag(fromSelect, fromImg);
   updateFlag(toSelect, toImg);
 });
-
-// 🔹 Convert Currency
 button.addEventListener("click", async (e) => {
   e.preventDefault();
-
   let amount = amountInput.value;
-
   if (amount === "" || amount <= 0) {
     amount = 1;
     amountInput.value = "1";
   }
-
   const fromCurrency = fromSelect.value;
   const toCurrency = toSelect.value;
-
   try {
     const response = await fetch(BASE_URL + fromCurrency);
     const data = await response.json();
-
     const rate = data.rates[toCurrency];
     const finalAmount = amount * rate;
-
     msg.innerText = `${amount} ${fromCurrency} = ${finalAmount.toFixed(2)} ${toCurrency}`;
   } catch (error) {
     msg.innerText = "Error fetching exchange rate!";
